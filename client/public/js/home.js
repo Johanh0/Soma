f141b61a626e6bc8b82eb19839ed6ed1eabbcca5
Johan Herrera <69118220+Johanh0@users.noreply.github.com>
2025-01-10 13:23:25 -0500

    Exercise Page is Done (#20)
    * Client folder updated. Also the basic routes for the pages were made
    
    * Handlebars are connected to the backend, also the structure for the styles and some partials (navbar and footer) and the hero of the homeage
    
    * Changes on the styles & basic API routes added. This version is fetching basic data from the third APIs
    
    * Working progress
    
    * working progress
    
    * Exercise page were added and started to working on it
    
    * working progress
    
    * Exercise page is done
    
    * Updates in the navbar and footer links and also BMI route,page,styles and js created. A basic structure in the BMI page were made

--- client/public/js/home.js
+++ client/public/js/home.js
@@ -1,8 +1,9 @@
-import { navbarToggle, themeToggle } from "/js/navbar.js";
+import { navbarToggle, themeToggle, storageTheme } from "/js/navbar.js";
 import footerYear from "/js/footer.js";
 
 // Trigger functions after the DOM were load
 document.addEventListener("DOMContentLoaded", () => {
+  storageTheme();
   themeToggle();
   navbarToggle();
   footerYear();
