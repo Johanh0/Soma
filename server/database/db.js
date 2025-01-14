6494eef00c219bd7cc7d3d5135618d9e5e7a9e19
Johan Herrera <johanherrera20000@gmail.com>
2025-01-14 11:02:30 -0500

    the website is almost done,  we need to fix the bug with the token

--- server/database/db.js
+++ server/database/db.js
@@ -1,29 +1,28 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
-const bcrypt = require('bcryptjs');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+const bcrypt = require("bcryptjs");
 
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection
-    .promise()
-    .query('SELECT * FROM users')
-    .then(([rows, fields]) => {
-        console.log(rows);
-    });
-    
-    
+  .promise()
+  .query("SELECT * FROM users")
+  .then(([rows, fields]) => {
+    // console.log(rows);
+  });
+
 // function addUser(firstName, lastName, email, password) {
 //     // Check if email already exists
 //         // query the db for the email
 //         // if the email already exists, return an error
-    
+
 //     // Otherwise, hash the password and insert the user into the db
 //     bcrypt.hash(password,10)
 //     .then((hashedPassword) => {return connection.promise().query('INSERT into users (first_name, last_name, email, password) VALUES (?,?,?,?)', [firstName, lastName, email, hashedPassword])})
@@ -41,13 +40,13 @@ connection
 //     connection.promise().query('SELECT * FROM users WHERE email = ?', [email])
 //     .then(([rows, fields]) => {
 //         // if the email doesn't exist, return an error
-        
+
 //         // otherwise, compare the entered password with the hashed password
 //         bcrypt.compare(enteredPassword, rows[0].password)
 //         .then((isMatch) => {
 //             if (isMatch) {
 //                 // log the user in, whatever that means
-//                 // jwt token, session, etc. 
+//                 // jwt token, session, etc.
 //                 console.log('Logged in');
 //             } else {
 //                 console.log('Incorrect password');
